da966866a6663b956340be8cb389bb692ffc7fab
iram-devs <iramshamim2@gmail.com>
2026-06-02 17:36:54 +0530

    Added few solutions

--- Week 1/Day 1/Solution.cpp
+++ Week 1/Day 1/Solution.cpp
@@ -29,3 +29,40 @@ public:
 };
 
 //Question 2- REMOVE DUPLICATES FROM SORTED ARRAY
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int replace=1;
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i-1]!=nums[i])
+            {
+                nums[replace]=nums[i];
+                replace++;
+            }
+        }
+        return replace;
+        
+    }
+};
+
+//Question 3- BEST TIME TO BUY AND SELL STOCK
+#include <vector>
+#include <climits>
+using namespace std;
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+         int maxProfit=0;
+         int minPrice=INT_MAX;
+         for(int i=0;i<prices.size();i++)
+         {
+            minPrice=min(minPrice,prices[i]);
+            maxProfit=max(maxProfit,prices[i]-minPrice);
+         }
+         return maxProfit;
+        
+    }
+};
