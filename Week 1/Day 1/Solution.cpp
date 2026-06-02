c9eebdcc6df9598dfeb3bce955dd986422670d3e
iram-devs <iramshamim2@gmail.com>
2026-06-02 11:53:05 +0530

    Added solutions

---
+++ Week 1/Day 1/Solution.cpp
@@ -0,0 +1,31 @@
+//Question 1 - TWO SUM
+#include <algorithm>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<pair<int,int>>v;
+        for(int i=0;i<nums.size();i++)
+        {
+            v.push_back({nums[i],i});
+        }
+        sort(v.begin(),v.end());
+        int left=0 , right=v.size()-1;
+        while(left<right)
+        {
+            int sum=v[left].first+v[right].first;
+            if(sum==target)
+            {
+                return{v[left].second,v[right].second};
+            }
+            else if(sum<target)left++;
+            else right--;
+            }
+            return{};
+        
+        
+    }
+};
+
+//Question 2- REMOVE DUPLICATES FROM SORTED ARRAY
