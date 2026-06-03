427d9fcd52d20e1e37e827125eb6b85a66c84e58
iram-devs <iramshamim2@gmail.com>
2026-06-03 09:47:48 +0530

    Added Day 3 solutions

--- Week 1/Day 3/Solution.cpp
+++ Week 1/Day 3/Solution.cpp
@@ -32,3 +32,21 @@ public:
 };
 
 //Question 3-CONTAINER WITH MOST WATER
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left=0 , right =height.size()-1;
+        int maxArea=0;
+        while(left<right)
+        {
+            int area = min(height[left],height[right]) * (right-left);
+            maxArea = max(area,maxArea);
+            if(height[left]<height[right]) left ++;
+            else right--;
+        }
+        return maxArea;
+    }
+};
