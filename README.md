be3385c836bb5a2c083b801a797f9c9010c12f16
iram-devs <iramshamim2@gmail.com>
2026-06-03 09:13:31 +0530

    Added details

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # Summer_Assignment_2401921540123
-This is my Summer Assignment.
-<br>
-It contains folders with name Week 1 , Week 2, Week 3 and Week 4.
-<br>
-Each folder has seperate subfolders daywise and a weekly OOPs folder. Every folder has a solution file 
-that contains solution of leetcode problems.
+Summer Assignment 2026.
---
+++ Week 1/Day 3/Solution.cpp
@@ -0,0 +1,34 @@
+//Question 1- MOVE ZEROES
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i=0;
+        for(int j=0;j<nums.size();j++){
+            if(nums[j]!=0){
+                swap(nums[i],nums[j]);
+                i++;
+            }
+        }
+    }
+};
+
+//Question 2- SQUARES OF A SORTED ARRAY
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector <int> square;
+        for(int i=0;i<nums.size();i++)
+        {
+            square.push_back(nums[i]*nums[i]);
+        }
+        sort(square.begin(),square.end());
+        return square;
+    }
+};
+
+//Question 3-CONTAINER WITH MOST WATER
