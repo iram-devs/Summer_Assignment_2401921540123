d2b2c7672b45df45ecfc96cc2f1ca9e8f1ba38d5
iram-devs <iramshamim2@gmail.com>
2026-06-25 17:52:49 +0530

    Added Day 4

---
+++ Week 4/Day 4/PathSum.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (root == NULL)
+        {
+            return false;
+        }
+        if(root->right == NULL && root->left == NULL)
+        {
+            return (root->val == targetSum);
+        }
+         return hasPathSum(root->right,targetSum-root->val)||hasPathSum(root->left , targetSum-root->val);
+    }
+};
